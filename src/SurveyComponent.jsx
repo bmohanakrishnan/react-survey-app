c59d1b95d8a45439ae7e9670a82fc1c5fdfdc6cb
bmohanakrishnan <mohanakrishnan.baskar@gmail.com>
2023-06-05 23:02:24 +0530

    Update SurveyComponent.jsx

--- src/SurveyComponent.jsx
+++ src/SurveyComponent.jsx
@@ -5,9 +5,23 @@ import "survey-core/defaultV2.min.css";
 import "./index.css";
 import { json } from "./json";
 
+
+
+export function call_api(item) {
+return fetch('https://hnhmywkgva.execute-api.us-east-1.amazonaws.com/sync-to-dynamo', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ item })
+})
+    .then(data => data.json())
+}
+
 function SurveyComponent() {
     const survey = new Model(json);
     survey.onComplete.add((sender, options) => {
+        call_api(JSON.stringify(sender.data, null, 3))
         console.log(JSON.stringify(sender.data, null, 3));
     });
     return (<Survey model={survey} />);
