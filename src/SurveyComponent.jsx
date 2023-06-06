c2ce2aa793ba5b658415ef0653bc0be07e94c614
bmohanakrishnan <mohanakrishnan.baskar@gmail.com>
2023-06-06 14:03:02 +0530

    code changes

--- src/SurveyComponent.jsx
+++ src/SurveyComponent.jsx
@@ -6,17 +6,12 @@ import "./index.css";
 import { json } from "./json";
 
 
-
 export function call_api(item) {
 return fetch('https://hnhmywkgva.execute-api.us-east-1.amazonaws.com/sync-to-dynamo', {
     method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({ item })
+    body: JSON.stringify({ item }),
+    mode: 'no-cors'
 })
-    .then(data => data.json())
 }
 
 function SurveyComponent() {
